77c359e563be1bb6dc5845f591a79d6fd4ae5542
Stephen Devine <ste@stedevine.com>
2015-12-05 12:37:07 -0800

    bug fix - search for double letter should terminate at line length - 1, not -2

--- day5/part1.py
+++ day5/part1.py
@@ -8,7 +8,7 @@ def hasThreeVowels(line):
     return False
 
 def hasDoubleLetter(line):
-        for i in range (0, len(line) -2, 1):
+        for i in range (0, len(line) -1, 1):
             if (line[i]==line[i+1]):
                 return True
         return False
