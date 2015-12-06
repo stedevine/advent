7dfac91e0a1912c6eccec5fcf48bdcd569c1a3c6
Stephen Devine <ste@stedevine.com>
2015-12-05 17:18:08 -0800

    clean up files, add comments

--- day1/part1.py
+++ day1/part1.py
@@ -1,3 +1,6 @@
+# To what floor do the instructions send santa?
+# ( -> move up
+# ) -> move down
 f = open('input.txt','r')
 data = f.read()
 total = data.count('(') - data.count(')')
--- day1/part2.py
+++ day1/part2.py
@@ -1,3 +1,9 @@
+# At which instruction does Santa first enter the basement
+# that is the current floor = -1
+# The first instruction is 1 (not 0)
+# ( -> move up
+# ) -> move down
+
 f = open('input.txt','r')
 data = f.read()
 currentFloor = 0
@@ -7,7 +13,9 @@ for character in data:
         currentFloor += 1
     else:
         currentFloor -= 1
+
     if (currentFloor == -1):
         print(index)
         break
+
     index += 1
