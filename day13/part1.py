4304ce0b567043854b5f2717198fbb844ac8bd14
Stephen Devine <ste@stedevine.com>
2015-12-19 19:16:54 -0800

    day 13

--- day13/part1.py
+++ day13/part1.py
@@ -11,19 +11,45 @@ def all_perms(elements):
                 # nb elements[0:1] works in both string and list contexts
                 yield perm[:i] + elements[0:1] + perm[i:]
 
-#def happiness_between_two_people(person1, person2, map):
-
+# map the first letter of each peron's name into their points relationship
 def get_relationship(line):
     words = line.split(" ")
     points = int(words[3])
-    if (words[4] == "lose"):
+    if (words[2] == "lose"):
         points = points * -1
-    return {'left': words[0], 'right' : words[-1].strip('.\n'), 'points': points}
-#print("\n".join(all_perms("ABDC")))
+    return {'left': words[0][0], 'right' : words[-1].strip('.\n')[0], 'points': points}
+
+# list comprehension to get the points
+def get_points(a, b, relationships):
+    points = 0
+    atob = next((x for x in relationships if x["left"] == a and x["right"] == b), None)
+#    print (atob)
+    if atob is not None:
+        points += atob["points"]
+
+    btoa = next((x for x in relationships if x["left"] == b and x["right"] == a), None)
+#    print (btoa)
+    if btoa is not None:
+        points += btoa["points"]
+
+    return points
+
 
 relationships = list()
 f = open('input.txt','r')
 for line in f:
     relationships.append(get_relationship(line))
 
-print(relationships)
+
+perms = list(all_perms("ABCDEFGM"))
+
+pointsList = []
+for perm in perms:
+    points=0
+    for i in range(0, len(perm)-1):
+        points += get_points(perm[i],perm[i+1], relationships)
+    points += get_points(perm[len(perm)-1],perm[0], relationships)
+    #print(points)
+    pointsList.append(points)
+
+print(max(pointsList))
