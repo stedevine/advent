b11380ae068fce186fd84e03060381ebb499ce01
Stephen Devine <ste@stedevine.com>
2016-02-14 23:45:39 -0800

    Day 2 - clean up and pylint

--- day2/part1.py
+++ day2/part1.py
@@ -1,20 +1,27 @@
-# Calculate the total area of required wrapping paper
-# Input is present dimensions: lxwxh (presents are perfect rectangular prisms)
-# Required area per present = surface area of present + extra
-# Extra = area of the smallest side
-f = open('input.txt','r')
-totalArea = 0
-for line in f:
-    # get dimensions from line
-    dimensions = line.split('x')
-    length = int(dimensions[0])
-    width = int(dimensions[1])
-    height = int(dimensions[2])
+"""This script solves the first question on http://adventofcode.com/day/2"""
 
-    # get areas of each of the three different sides
-    a = length * width
-    b = width * height
-    c = height * length
-    extra = min(a, min(b, c))
-    totalArea = totalArea + (2 * a) + (2 * b) + (2 * c) + extra
-print(totalArea)
+def calculate_total_area(file_name):
+    """Calculate the total area of required wrapping paper"""
+    # Input is present dimensions: lxwxh
+    # Presents are perfect rectangular prisms
+    # Required area per present = surface area of present + extra
+    # Extra = area of the smallest side
+    total_area = 0
+    for present in tuple(open(file_name, 'r')):
+        dimensions = present.split('x')
+        length = int(dimensions[0])
+        width = int(dimensions[1])
+        height = int(dimensions[2])
+
+        # get areas of each of the three different faces
+        face_a = length * width
+        face_b = width * height
+        face_c = height * length
+
+        extra = min(face_a, min(face_b, face_c))
+        present_area = (2 * face_a) + (2 * face_b) + (2 * face_c) + extra
+        total_area = total_area + present_area
+
+    return total_area
+
+print(calculate_total_area('input.txt'))
--- day2/part2.py
+++ day2/part2.py
@@ -1,21 +1,26 @@
-# Get total length of required ribbon for same presents as part1
-# ribbon required for each present is perimeter of smallest side + bow length
-# the bow length is equal to the volume (except its in one dimension)
-f = open('input.txt','r')
-totalLength = 0
-for line in f:
-    # get dimensions from file
-    dimensions = line.split('x')
-    length = int(dimensions[0])
-    width = int(dimensions[1])
-    height = int(dimensions[2])
+"""This script solves the second question on http://adventofcode.com/day/2"""
 
-    perimeter1 = (2 * length) + (2 * width)
-    perimeter2 = (2 * width) + (2 * height)
-    perimeter3 = (2 * height) + (2 * length)
+def calculate_ribbon_length(file_name):
+    """Get total length of required ribbon for presents"""
+    # ribbon required for each present is :
+    # perimeter of smallest side + bow length
+    # the bow length is equal to the volume
+    # (except it is in one dimension)
+    total_length = 0
+    for present in tuple(open(file_name, 'r')):
+        dimensions = present.split('x')
+        length = int(dimensions[0])
+        width = int(dimensions[1])
+        height = int(dimensions[2])
 
-    ribbon = min(perimeter1, min(perimeter2, perimeter3))
-    bow = length * width * height
-    totalLength += ribbon + bow
+        perimeter1 = (2 * length) + (2 * width)
+        perimeter2 = (2 * width) + (2 * height)
+        perimeter3 = (2 * height) + (2 * length)
 
-print(totalLength)
+        ribbon = min(perimeter1, min(perimeter2, perimeter3))
+        bow = length * width * height
+        total_length += ribbon + bow
+
+    return total_length
+
+print(calculate_ribbon_length('input.txt'))
